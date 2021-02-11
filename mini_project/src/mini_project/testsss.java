13289c9d00513d8f4f3ff536d84125ffab038368
cksgmlzlzl <31941829+cksgmlzlzl@users.noreply.github.com>
2021-02-11 16:50:59 +0900

    sssss

--- mini_project/src/mini_project/testsss.java
+++ mini_project/src/mini_project/testsss.java
@@ -2,6 +2,6 @@ package mini_project;
 
 public class testsss {
 	public static void main(String[] args) {
-		System.out.println("Hello wodfrldxx");
+		System.out.println("Hello wodfrldxxsa");
 	}
 }
